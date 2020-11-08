818f0bfa4b78871070688b37f8bfab57cf9f98b3
Paul Piggott <Flying-piggott@hotmail.com>
2020-11-08 18:58:59 -0500

    Trying to use DB

--- index.php
+++ index.php
@@ -2,34 +2,23 @@
 include_once(__DIR__ . "/classes/utils/DB.php");
 include_once(__DIR__ . "/classes/utils/DBConn.php");
 
-$classes = get_declared_classes();
-phpinfo();
+$db = DB::getInstance();
 
-echo "\n\n\n" . var_dump($classes) . "\n\n\n";
-//use classes\utils\DB;
-//
-//
-//$db = DB::getInstance();
-//
-//$sql = <<<EOD
-//SHOW tables;
-//EOD;
-//
-///* @var $dbConn \classes\utils\DBConn */
-//try {
-//
-//    $dbConn = $db->getConnection();
-//    /* @var $statement PDOStatement */
-//    $statement = $dbConn->prepare($sql);
-//    $statement->execute();
-//    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-//    $json = json_encode($results);
-//    echo print_r($json);
-//
-//} catch (Exception $e) {
-//    echo "RUH ROH";
-//}
+$sql = <<<EOD
+SHOW tables;
+EOD;
 
+/* @var $dbConn \classes\utils\DBConn */
+try {
 
+    $dbConn = $db->getConnection();
+    /* @var $statement PDOStatement */
+    $statement = $dbConn->prepare($sql);
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $json = json_encode($results);
+    echo print_r($json);
 
-
+} catch (Exception $e) {
+    echo "RUH ROH";
+}
