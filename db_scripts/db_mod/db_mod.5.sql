61e35f439942c21374e22c4662c6c7c6446a0768
Paul Piggott <Flying-piggott@hotmail.com>
2020-11-29 15:12:48 -0500

    Add building to condo_unit

--- db_scripts/db_mod/db_mod.5.sql
+++ db_scripts/db_mod/db_mod.5.sql
@@ -1,1 +1,5 @@
 alter table building add column name text not null;
+
+alter table condo_unit add column buildingid INT;
+alter table condo_unit add constraint foreign key (buildingid) references building(id);
+
