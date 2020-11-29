72d44da1c0dc2ca4444297ab0cc4b2d2874836be
Paul Piggott <Flying-piggott@hotmail.com>
2020-11-29 15:38:11 -0500

    Added not null constraint to building.associationId to force buildings to be part of associations

--- db_scripts/db_mod/db_mod.5.sql
+++ db_scripts/db_mod/db_mod.5.sql
@@ -3,3 +3,4 @@ alter table building add column name text not null;
 alter table condo_unit add column buildingid INT;
 alter table condo_unit add constraint foreign key (buildingid) references building(id);
 
+alter table building modify buildingid INT NOT NULL;
